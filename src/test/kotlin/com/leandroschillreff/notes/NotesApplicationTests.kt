899e8008fe95ca8d33f48daed0b8cb3dd2975518
Leandro Schillreff <leandroschillreff@gmail.com>
2025-07-09 18:00:16 -0400

    first commit

---
+++ src/test/kotlin/com/leandroschillreff/notes/NotesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leandroschillreff.notes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
