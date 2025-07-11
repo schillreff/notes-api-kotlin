ad3bc9ff447f2a79f1d9395edee158ebe9073ba5
Leandro Schillreff <leandroschillreff@gmail.com>
2025-07-11 16:26:18 -0400

    feat: implement NoteController with save and findByOwnerId endpoints

--- src/main/kotlin/com/leandroschillreff/notes/controllers/NoteController.kt
+++ src/main/kotlin/com/leandroschillreff/notes/controllers/NoteController.kt
@@ -1,4 +1,67 @@
 package com.leandroschillreff.notes.controllers
 
-class NoteController {
+import com.leandroschillreff.notes.controllers.NoteController.NoteResponse
+import com.leandroschillreff.notes.database.model.Note
+import com.leandroschillreff.notes.database.repository.NoteRepository
+import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.*
+import java.time.Instant
+
+@RestController
+@RequestMapping("/notes")
+class NoteController(
+    private val repository: NoteRepository
+) {
+
+    data class NoteRequest(
+        val id: String?,
+        val title: String,
+        val content: String,
+        val color: Long
+    )
+
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant
+    )
+
+    @PostMapping
+    fun save(
+        @RequestBody body: NoteRequest
+    ): NoteResponse {
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId()
+            )
+        )
+
+        return note.toResponse()
+    }
+
+    @GetMapping
+    fun findByOwnerId(
+        @RequestParam(required = false) ownerId: String
+    ): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+            it.toResponse()
+        }
+    }
+}
+
+private fun Note.toResponse(): NoteController.NoteResponse {
+    return NoteResponse(
+        id = id.toHexString(),
+        title = title,
+        content = content,
+        color = color,
+        createdAt = createdAt
+    )
 }
