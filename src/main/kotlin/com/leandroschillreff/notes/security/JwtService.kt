94b9c38d6ffc013eb8f3f4a7080b5bca2f133276
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-02 12:31:39 -0400

    feat: initialize secretKey in JwtService for JWT signing

--- src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
+++ src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
@@ -1,7 +1,10 @@
 package com.leandroschillreff.notes.security
 
+import io.jsonwebtoken.security.Keys
 import org.springframework.stereotype.Service
 
 @Service
 class JwtService {
+
+    private val secretKey = Keys.hmacShaKeyFor()
 }
