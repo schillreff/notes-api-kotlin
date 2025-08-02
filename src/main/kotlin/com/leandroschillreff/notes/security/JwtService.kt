f2628fa71fb2e8daee003803468d0e35c581c5b7
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-02 12:35:05 -0400

    feat: refactor JwtService to include JWT secret as a constructor parameter

--- src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
+++ src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
@@ -1,10 +1,11 @@
 package com.leandroschillreff.notes.security
 
 import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class JwtService {
+class JwtService(@Value("JWT_SECRET_BASE64") private val jwtSecret: String) {
 
     private val secretKey = Keys.hmacShaKeyFor()
 }
