fcf68624b180386b931c1e4e1359c80c9ef92c78
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-02 12:35:51 -0400

    feat: decode JWT secret in JwtService for secretKey initialization

--- src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
+++ src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
@@ -3,9 +3,10 @@ package com.leandroschillreff.notes.security
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.util.Base64
 
 @Service
 class JwtService(@Value("JWT_SECRET_BASE64") private val jwtSecret: String) {
 
-    private val secretKey = Keys.hmacShaKeyFor()
+    private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
 }
