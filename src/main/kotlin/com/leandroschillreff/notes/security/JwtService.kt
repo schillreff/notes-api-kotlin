5ba94cd7a53ef819f8bf248bede3f0230f0b2cd7
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-02 12:37:48 -0400

    feat: add accessTokenValidityMs property to JwtService for token expiration management

--- src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
+++ src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
@@ -3,10 +3,11 @@ package com.leandroschillreff.notes.security
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.util.Base64
+import java.util.*
 
 @Service
 class JwtService(@Value("JWT_SECRET_BASE64") private val jwtSecret: String) {
 
     private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
+    private val accessTokenValidityMs = 15L * 60L * 1000L
 }
