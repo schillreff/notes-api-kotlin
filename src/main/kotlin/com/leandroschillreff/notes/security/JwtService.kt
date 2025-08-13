35408e0fe559775f618f1244377d2578e9b9da04
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-13 18:03:18 -0400

    feat: refactor JwtService to use correct JWT secret property and enhance token generation method

--- src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
+++ src/main/kotlin/com/leandroschillreff/notes/security/JwtService.kt
@@ -1,13 +1,27 @@
 package com.leandroschillreff.notes.security
 
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class JwtService(@Value("JWT_SECRET_BASE64") private val jwtSecret: String) {
+class JwtService(@Value("\${jwt.secret}") private val jwtSecret: String) {
 
     private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
     private val accessTokenValidityMs = 15L * 60L * 1000L
+    val refreshTokenValidityMs = 24L * 60L * 60L * 1000L
+
+    private fun generateToken(
+        userId: String, type: String, expiry: Long
+    ): String {
+        val now = Date()
+        val expiryDate = Date(now.time + expiry)
+        return Jwts.builder().subject(userId).claim("type", type).issuedAt(now).expiration(expiryDate)
+            .signWith(secretKey, Jwts.SIG.HS256).compact()
+    }
+
+   
 }
