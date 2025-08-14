ae92daa06a95e435a4d384b1eca54442de4c4ce2
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-14 15:00:39 -0400

    feat: implement AuthService class for user authentication and token generation

--- src/main/kotlin/com/leandroschillreff/notes/security/AuthService.kt
+++ src/main/kotlin/com/leandroschillreff/notes/security/AuthService.kt
@@ -1,4 +1,35 @@
 package com.leandroschillreff.notes.security
 
-class AuthService {
+import com.leandroschillreff.notes.database.model.User
+import com.leandroschillreff.notes.database.repository.UserRepository
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService(
+    private val jwtService: JwtService,
+    private val userRepository: UserRepository,
+    private val hashEncoder: HashEncoder
+) {
+    data class TokenPair(
+        val accessToken: String,
+        val refreshToken: String
+    )
+
+    fun register(email: String, password: String): User {
+        return userRepository.save(User(email = email, hashedPassword = hashEncoder.encode(password)))
+    }
+
+    fun login(email: String, password: String): TokenPair {
+        val user = userRepository.findByEmail(email) ?: throw BadCredentialsException("Invalid credentials.")
+
+        if (!hashEncoder.matches(password, user.hashedPassword)) {
+            throw BadCredentialsException("Invalid credentials.")
+        }
+
+        val newAccessToken = jwtService.generateAccessToken(user.id.toHexString())
+        val newRefreshToken = jwtService.generateRefreshToken(user.id.toHexString())
+
+        return TokenPair(accessToken = newAccessToken, refreshToken = newRefreshToken)
+    }
 }
