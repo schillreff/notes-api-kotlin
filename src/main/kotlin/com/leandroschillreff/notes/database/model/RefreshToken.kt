542fa03ac1f42fa5e5dc60713833512766ec0cc0
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-21 15:23:23 -0400

    feat: add userId field to RefreshToken data class for MongoDB mapping

--- src/main/kotlin/com/leandroschillreff/notes/database/model/RefreshToken.kt
+++ src/main/kotlin/com/leandroschillreff/notes/database/model/RefreshToken.kt
@@ -1,6 +1,9 @@
 package com.leandroschillreff.notes.database.model
 
+import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("refresh_token")
-data class RefreshToken()
+data class RefreshToken(
+    val userId: ObjectId,
+)
