4a97ffe3929e5c783cdca1e316378fe2cc2d8b5d
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-21 15:23:59 -0400

    feat: add expiresAt field to RefreshToken data class for expiration tracking

--- src/main/kotlin/com/leandroschillreff/notes/database/model/RefreshToken.kt
+++ src/main/kotlin/com/leandroschillreff/notes/database/model/RefreshToken.kt
@@ -2,8 +2,11 @@ package com.leandroschillreff.notes.database.model
 
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
 
 @Document("refresh_token")
 data class RefreshToken(
     val userId: ObjectId,
+    val expiresAt: Instant,
+
 )
