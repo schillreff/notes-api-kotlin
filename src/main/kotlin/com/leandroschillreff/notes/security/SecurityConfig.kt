ae76d4d53ca29aa1b75a1c3105cb0b9db3cc95b3
Leandro Schillreff <leandroschillreff@gmail.com>
2025-09-08 20:25:45 -0400

    feat: enhance SecurityConfig to include custom authentication entry point and JWT filter

--- src/main/kotlin/com/leandroschillreff/notes/security/SecurityConfig.kt
+++ src/main/kotlin/com/leandroschillreff/notes/security/SecurityConfig.kt
@@ -3,12 +3,17 @@ package com.leandroschillreff.notes.security
 import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthFilter: JwtAuthFilter
+) {
 
     @Bean
     fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
@@ -25,6 +30,12 @@ class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .authenticated()
-            }.build()
+            }
+            .exceptionHandling { configurer ->
+                configurer
+                    .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            }
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .build()
     }
 }
